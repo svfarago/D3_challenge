fb5d4d522d988134d0c523ae3c28c463c383e965
gitsusan <svfarago@gmail.com>
2021-04-03 14:52:58 -0600

    More code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 // POTATO - if errors occur then move this section of code down.
 d3.csv("assets/data/data.csv").then(function(CensusData) {
     CensusData.forEach(function(data) { //data could be "d"
-      data.age = +data.age;  //assign whatever data type is in data.age back as an integer (that's what the + does)  | in other words cast the data into another type
+      data.obesity = +data.obesity;  //assign whatever data type is in data.obesity back as an integer (that's what the + does)  | in other words cast the data into another type
       data.smokes = +data.smokes;
       console.log(data);
  }); // ends THEn call (put a note of what you are ending - helps keep track)
@@ -12,13 +12,13 @@ d3.csv("assets/data/data.csv").then(function(CensusData) {
 
 // SVG Container - Way to calculate margins and chart height/width using variables
 // Potato - can any of this be hardcoded to var svg below to simplify code?
-var svgHeight = 600;
-var svgWidth = 800;
+var svgHeight = 800;
+var svgWidth = 900;
 
 var margin = {
   top: 20,
   bottom: 60,
-  left: 20,
+  left: 40,
   right: 20,
 };
 
@@ -26,7 +26,7 @@ var height = svgHeight - margin.top - margin.bottom;
 var width = svgWidth - margin.left - margin.right;
 
 
-// ====Create an SVG wrapper so chart can be dynamically resized,append an SVG group that will hold chart and set margins=====
+//==== Create an SVG wrapper so chart can be dynamically resized,append an SVG group that will hold chart and set margins=====
 var svg = d3
   .select("#scatter")  // divID "scatter" in HTML to place chart
   .append("svg")
@@ -40,51 +40,75 @@ var svg = d3
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-  // ==============Create Scales====================
+  // ========= Create Scales ====================
   // Exercise 16/2/4 on 4/1 and 16/2/5 on 4/3
   // d, i = data and index
-  //POTATO - why "const" and not "var"
-  const xScale = d3.scaleLinear() //scaleLinear = for age column
-    .domain(d3.extent(CensusData, d => d.age)) // use d3.extend to return min and max range of data.age
+  //POTATO - changed "const" to "var" to play around with it
+  var xScale = d3.scaleLinear() //scaleLinear = for obesity column
+    .domain(d3.extent(CensusData, d => d.obesity)) // use d3.extend to return min and max range of data.obesity
     .range([0, width])
-    //.nice(); // rounds values POTATO needed?
+    .nice(); // rounds axis values
 
-  const yScale = d3.scaleLinear() //scaleLinear = for smokes column
+  var yScale = d3.scaleLinear() //scaleLinear = for smokes column
     //POTATO why are both scaleLinear and one not scaleBand? exercise 16/2/6; maybe because we haven't created axes yet of  - assign 16/2/6
     //var yAxis = d3.axisLeft(yScale);
     //var xAxis = d3.axisBottom(xScale);
     .domain(d3.extent(CensusData, d => d.smokes))
     //.domain([6,d3.max(CensusData, d => d.smokes)]) // use d3.max to return max range of data.smokes
     .range([height, 0])
-    //.nice(); // rounds values POTATO needed?
+    .nice(); // rounds axis values
   
 
-//============Generate scatter plot=========
+//=======Generate scatter plot=========
     chartGroup.selectAll("circle")
     .data(CensusData)
     .enter()
     .append("circle")
-    .attr("cx", d=>xScale(d.age)) // Replaces data with data.age on xScale
+    .attr("cx", d=>xScale(d.obesity)) // Replaces data with data.obesity on xScale
     .attr("cy", d=>yScale(d.smokes)) // Replaces data with data.smokes on yScale
-    .attr("r", "15")
-    .style("fill", "#5f96b6")
+    .attr("r", "18")
+    .style("fill", "#164270")
+
+  //========Create Axes=========================
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
 
 
+//=======Append axes to the chartGroup==========
+  chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+  chartGroup.append("g").call(yAxis);
+
+//=======Add labels to datapoints=========
+chartGroup.append("g")
+  .selectAll("text")
+  .data(CensusData)
+  .enter()
+  .append("text")
+  .text(d=>d.abbr) // Changes full state name to state abbreviation
+  .attr("x",d=>xScale(d.obesity))
+  .attr("y",d=>yScale(d.smokes))
+  .classed(".stateText", true)
+  .attr("text-anchor", "middle")
+  .attr("alignment-baseline", "central"); // centers State in circles
+  
+  //============add axes titles=========
+  chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 10})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+
+//        .style("font-weight", "bold")
+        .text("Population % Obesity");
+
+        chartGroup.append("text")
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "14px")
+//        .style("font-weight", "bold")
+        .attr("transform", "rotate(-90)")
+        .text("Smokers (%)");
+
+  
 });
 
-
-
-
-
-// This code snippet might be useful later - embed in Axes labels
-    // Create tooltip in the chart
-    //chartGroup.call(toolTip);
-
-    // Create event listeners to display and hide the tooltip
-    //circlesGroup.on("mouseover", function(data) {
-    //  toolTip.show(data, this);
-   // })
-      // onmouseout event
-    //  .on("mouseout", function(data, index) {
-    //    toolTip.hide(data);
-    //  });
